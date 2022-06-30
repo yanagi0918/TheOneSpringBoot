758cec4e9eca3e3e5c641d2b1db2696efc71681d
Vincent Lian <bir0405work@gmail.com>
2022-06-30 16:52:56 +0800

    20220630_1

--- src/main/java/com/theone/springboot/controller/CourseController.java
+++ src/main/java/com/theone/springboot/controller/CourseController.java
@@ -12,12 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.theone.springboot.entity.CourseBean;
@@ -25,6 +20,7 @@ import com.theone.springboot.service.CourseService;
 
 
 @Controller
+@RequestMapping("/dashboard")
 public class CourseController  {
 	
 	private final CourseService courseService;
