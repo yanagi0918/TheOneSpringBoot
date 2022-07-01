d6cfa14aba09abc8dfa496cd25baff8480f97db2
yc20150701 <yc20150701@gmail.com>
2022-07-01 11:32:47 +0800

    event test JPS naming rule

--- src/test/java/com/theone/springboot/TheOneSpringBootApplicationTests.java
+++ src/test/java/com/theone/springboot/TheOneSpringBootApplicationTests.java
@@ -1,13 +1,29 @@
 package com.theone.springboot;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.theone.springboot.entity.Event;
+import com.theone.springboot.repository.EventDao;
 
 @SpringBootTest
 class TheOneSpringBootApplicationTests {
-
+	
+	@Autowired
+	EventDao eventDao;
+	
 	@Test
-	void contextLoads() {
+	public void testJPA() {
+		List<Event> events = eventDao.findByCompId("12345678");
+		System.out.println("==================================");
+		for (Event event : events) {
+			System.out.println(event);
+		}
+		System.out.println("==================================");
 	}
+
 
 }
