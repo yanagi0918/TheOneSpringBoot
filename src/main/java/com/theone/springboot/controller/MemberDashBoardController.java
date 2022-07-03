ab78f4c5f091df1f22fdd5a9bcc7ab139f1b8cc7
yusiou0 <linyu19941229@gmail.com>
2022-07-03 21:51:49 +0800

    id check test

--- src/main/java/com/theone/springboot/controller/MemberDashBoardController.java
+++ src/main/java/com/theone/springboot/controller/MemberDashBoardController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MultipartFile;
@@ -94,5 +95,13 @@ public class MemberDashBoardController {
     }
 	
 	
+	@PostMapping(value = "/CheckMember")
+	public @ResponseBody boolean checkMember(@RequestParam String userid) {
+		Member member = memberService.getByUserid(userid);
+		if(member == null) {
+			return true;
+		}
+		return false;
+	}
 	
 }
