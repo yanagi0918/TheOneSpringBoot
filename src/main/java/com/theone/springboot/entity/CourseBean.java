3cefa7a5e5d6b3553aaa83d9ebdc90930171d343
Vincent Lian <bir0405work@gmail.com>
2022-07-06 22:01:25 +0800

    後台課程審核狀態功能

--- src/main/java/com/theone/springboot/entity/CourseBean.java
+++ src/main/java/com/theone/springboot/entity/CourseBean.java
@@ -2,12 +2,9 @@ package com.theone.springboot.entity;
 
 import java.sql.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
@@ -17,125 +14,134 @@ import org.springframework.stereotype.Component;
 @Table(name = "Course_Table")
 @Component
 public class CourseBean {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer courseNo;
-	private String courseCategory;
-	private String courseName;
-	private String courseIntroduction;
-	private String lecturer;
-	@Column(columnDefinition = "Date", name = "launch_date")
-	private Date date;
-	private String coursePicUrl;
-	private String courseVedioUrl;
-	private Double score;
-	private Integer price;
-	
-	public CourseBean() {
-	}
-	
-	public CourseBean(Integer courseNo, String courseCategory, String courseName, String courseIntroduction,
-			String lecturer, Date date, String coursePicUrl, String courseVedioUrl, Double score, Integer price) {
-		
-		this.courseNo = courseNo;
-		this.courseCategory = courseCategory;
-		this.courseName = courseName;
-		this.courseIntroduction = courseIntroduction;
-		this.lecturer = lecturer;
-		this.date = date;
-		this.coursePicUrl = coursePicUrl;
-		this.courseVedioUrl = courseVedioUrl;
-		this.score = score;
-		this.price = price;
-	}
 
-	public Integer getCourseNo() {
-		return courseNo;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer courseNo;
+    private String courseCategory;
+    private String courseName;
+    private String courseIntroduction;
+    private String lecturer;
+    @NotBlank
+    private String status;
+    @Column(columnDefinition = "Date", name = "launch_date")
+    private Date date;
+    private String coursePicUrl;
+    private String courseVedioUrl;
+    private Double score;
+    private Integer price;
 
-	public void setCourseNo(Integer courseNo) {
-		this.courseNo = courseNo;
-	}
+    public CourseBean() {
+    }
 
-	public String getCourseCategory() {
-		return courseCategory;
-	}
+    public CourseBean(Integer courseNo, String courseCategory, String courseName, String courseIntroduction, String lecturer, String status, Date date, String coursePicUrl, String courseVedioUrl, Double score, Integer price) {
+        this.courseNo = courseNo;
+        this.courseCategory = courseCategory;
+        this.courseName = courseName;
+        this.courseIntroduction = courseIntroduction;
+        this.lecturer = lecturer;
+        this.status = status;
+        this.date = date;
+        this.coursePicUrl = coursePicUrl;
+        this.courseVedioUrl = courseVedioUrl;
+        this.score = score;
+        this.price = price;
+    }
 
-	public void setCourseCategory(String courseCategory) {
-		this.courseCategory = courseCategory;
-	}
+    public Integer getCourseNo() {
+        return courseNo;
+    }
 
-	public String getCourseName() {
-		return courseName;
-	}
+    public void setCourseNo(Integer courseNo) {
+        this.courseNo = courseNo;
+    }
 
-	public void setCourseName(String courseName) {
-		this.courseName = courseName;
-	}
+    public String getCourseCategory() {
+        return courseCategory;
+    }
 
-	public String getCourseIntroduction() {
-		return courseIntroduction;
-	}
+    public void setCourseCategory(String courseCategory) {
+        this.courseCategory = courseCategory;
+    }
 
-	public void setCourseIntroduction(String courseIntroduction) {
-		this.courseIntroduction = courseIntroduction;
-	}
+    public String getCourseName() {
+        return courseName;
+    }
 
-	public String getLecturer() {
-		return lecturer;
-	}
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
 
-	public void setLecturer(String lecturer) {
-		this.lecturer = lecturer;
-	}
+    public String getStatus() {
+        return status;
+    }
 
-	public Date getDate() {
-		return date;
-	}
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
+    public String getCourseIntroduction() {
+        return courseIntroduction;
+    }
 
-	public String getCoursePicUrl() {
-		return coursePicUrl;
-	}
+    public void setCourseIntroduction(String courseIntroduction) {
+        this.courseIntroduction = courseIntroduction;
+    }
 
-	public void setCoursePicUrl(String coursePicUrl) {
-		this.coursePicUrl = coursePicUrl;
-	}
+    public String getLecturer() {
+        return lecturer;
+    }
 
-	public String getCourseVedioUrl() {
-		return courseVedioUrl;
-	}
+    public void setLecturer(String lecturer) {
+        this.lecturer = lecturer;
+    }
 
-	public void setCourseVedioUrl(String courseVedioUrl) {
-		this.courseVedioUrl = courseVedioUrl;
-	}
+    public Date getDate() {
+        return date;
+    }
 
-	public Double getScore() {
-		return score;
-	}
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
-	public void setScore(Double score) {
-		this.score = score;
-	}
+    public String getCoursePicUrl() {
+        return coursePicUrl;
+    }
 
-	public Integer getPrice() {
-		return price;
-	}
+    public void setCoursePicUrl(String coursePicUrl) {
+        this.coursePicUrl = coursePicUrl;
+    }
 
-	public void setPrice(Integer price) {
-		this.price = price;
-	}
+    public String getCourseVedioUrl() {
+        return courseVedioUrl;
+    }
 
-	@Override
-	public String toString() {
-		return "CourseBean [courseNo=" + courseNo + ", courseCategory=" + courseCategory + ", courseName=" + courseName
-				+ ", courseIntroduction=" + courseIntroduction + ", lecturer=" + lecturer + ", date=" + date
-				+ ", coursePicUrl=" + coursePicUrl + ", courseVedioUrl=" + courseVedioUrl + ", score=" + score
-				+ ", price=" + price + "]";
-	}
-	
+    public void setCourseVedioUrl(String courseVedioUrl) {
+        this.courseVedioUrl = courseVedioUrl;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseBean [courseNo=" + courseNo + ", courseCategory=" + courseCategory + ", courseName=" + courseName
+                + ", courseIntroduction=" + courseIntroduction + ", lecturer=" + lecturer + ", date=" + date
+                + ", coursePicUrl=" + coursePicUrl + ", courseVedioUrl=" + courseVedioUrl + ", score=" + score
+                + ", price=" + price + "]";
+    }
+
 }
