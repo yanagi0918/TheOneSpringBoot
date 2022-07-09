154c45ea6fb7c724d36e688cdf266ce97723a19d
chu13826 <zxc89516@gmail.com>
2022-07-10 00:15:57 +0800

    login update
    quick input

--- src/main/resources/static/js/login/login.js
+++ src/main/resources/static/js/login/login.js
@@ -4,29 +4,29 @@ const cards = document.querySelectorAll('.card');
 -----------------------------------------*/
 const btns = document.querySelectorAll('.js-btn');
 btns.forEach(btn => {
-  btn.addEventListener('click', on_btn_click, true);
-  btn.addEventListener('touch', on_btn_click, true);
+	btn.addEventListener('click', on_btn_click, true);
+	btn.addEventListener('touch', on_btn_click, true);
 });
 
 function on_btn_click(e) {
-  const nextID = e.currentTarget.getAttribute('data-target');
-  const next = document.getElementById(nextID);
-  if (!next) return;
-  bg_change(nextID);
-  view_change(next);
-  return false;
+	const nextID = e.currentTarget.getAttribute('data-target');
+	const next = document.getElementById(nextID);
+	if (!next) return;
+	bg_change(nextID);
+	view_change(next);
+	return false;
 }
 
 /* Add class to the body */
 function bg_change(next) {
-  document.body.className = '';
-  document.body.classList.add('is-' + next);
+	document.body.className = '';
+	document.body.classList.add('is-' + next);
 }
 
 /* Add class to a card */
 function view_change(next) {
-  cards.forEach(card => {card.classList.remove('is-show');});
-  next.classList.add('is-show');
+	cards.forEach(card => { card.classList.remove('is-show'); });
+	next.classList.add('is-show');
 }
 
 
@@ -49,3 +49,20 @@ Cswitchers.forEach(item => {
 	})
 })
 
+
+/* 浮動提示 */
+$(function() {
+	$('[data-toggle="tooltip"]').tooltip();
+});
+
+//一鍵輸入
+$(function() {
+	$('#MemberQuick').click(function() {
+		$('#username').val('A123456789')
+		$('#password').val('123456')
+	})
+	$('#Admin').click(function() {
+		$('#username').val('admin')
+		$('#password').val('12345')
+	})
+});
