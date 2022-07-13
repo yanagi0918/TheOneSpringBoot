6cbb2ac52f2295eb224d3c93e9a008afeffb0a7f
Vincent Lian <bir0405work@gmail.com>
2022-07-13 15:47:01 +0800

    Update CourseUserController.java

--- src/main/java/com/theone/springboot/controller/CourseUserController.java
+++ src/main/java/com/theone/springboot/controller/CourseUserController.java
@@ -6,6 +6,7 @@ import com.theone.springboot.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
