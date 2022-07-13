586633f6d4b9a24a7c4dbb22ca8acc5cded01159
chu13826 <zxc89516@gmail.com>
2022-07-13 11:46:27 +0800

    Comment Update chart
    update chart.js in dashboard

--- src/main/java/com/theone/springboot/controller/CommentDashBoardController.java
+++ src/main/java/com/theone/springboot/controller/CommentDashBoardController.java
@@ -1,5 +1,9 @@
 package com.theone.springboot.controller;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,19 +13,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.theone.springboot.entity.Comment;
 import com.theone.springboot.service.CommentService;
 
-
-
 @Controller
 @RequestMapping("/dashboard")
 public class CommentDashBoardController {
-	
+
 	@Autowired
 	CommentService commentService;
-	
+
 	// 所有評論list
 	@RequestMapping("/comments")
 	public String listComments(Model model) {
@@ -42,7 +45,8 @@ public class CommentDashBoardController {
 		commentService.deleteById(id);
 		return "redirect:./comments";
 	}
-	//更新評論
+
+	// 更新評論
 	@RequestMapping("/CommentUpdate")
 	public String updateComment(@ModelAttribute("comment") Comment comment) {
 		commentService.saveOrUpdate(comment);
@@ -54,7 +58,7 @@ public class CommentDashBoardController {
 	public String showCommentForm(@ModelAttribute("comment") Comment comment) {
 		return "comment_dashboard/commentform";
 	}
-	
+
 	// 送出新增評價的空白表單
 	@RequestMapping("/CommentEdit/{id}")
 	public String showEditForm(@PathVariable("id") Integer id, Model model) {
@@ -62,7 +66,7 @@ public class CommentDashBoardController {
 		model.addAttribute("comment", comment);
 		return "comment_dashboard/commentform";
 	}
-	
+
 	// 送出評價的詳細資料
 	@RequestMapping("/CommentDetail/{id}")
 	public String showDetailForm(@PathVariable("id") Integer id, Model model) {
@@ -71,10 +75,25 @@ public class CommentDashBoardController {
 		return "comment_dashboard/commentdetail";
 	}
 
-	// 確認新增評論的資料
-	@RequestMapping("/CommentConfirm")
-	public String showConfirmForm(@ModelAttribute("comment") Comment comment) {
-		return "comment_dashboard/commentdetail";
+	@RequestMapping("/comments/jobtypejson")
+	@ResponseBody
+	public int[] getJobTypeJson() {
+		int[] jobtype = { 0, 0, 0, 0 };
+		List<Comment> allComments = commentService.findAll();
+		for (Comment comment : allComments) {
+
+			if (comment.getJob_description() == "全職") {
+				jobtype[0]++;
+			} else if (comment.getJob_description() == "兼職") {
+				jobtype[1]++;
+			} else if (comment.getJob_description() == "工讀") {
+				jobtype[2]++;
+			} else if (comment.getJob_description() == "實習") {
+				jobtype[3]++;
+			}
+		}
+
+		return jobtype;
 	}
 
 }
