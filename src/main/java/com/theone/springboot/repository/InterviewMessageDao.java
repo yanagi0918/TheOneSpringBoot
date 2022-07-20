ee1cba34cbcaf9c20cfeb66c5eb25cc257d5865b
iamgmchiu <iamgmchiu@gmail.com>
2022-07-20 21:09:34 +0800

    完成版本

--- src/main/java/com/theone/springboot/repository/InterviewMessageDao.java
+++ src/main/java/com/theone/springboot/repository/InterviewMessageDao.java
@@ -2,7 +2,6 @@ package com.theone.springboot.repository;
 
 import java.util.List;
 
-import org.hibernate.annotations.SQLInsert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.theone.springboot.entity.InterviewMessage;
@@ -12,7 +11,8 @@ import com.theone.springboot.entity.InterviewMessage;
 public interface InterviewMessageDao extends JpaRepository<InterviewMessage, Integer> {
 
 	List<InterviewMessage> findByInterviewCvNo(Integer cv_no);
-
+	
+	
 //	@SQLInsert(sql = "INSERT INTO Interview_Message(name,words,time)"+
 //							"value(#{name},#{words},#{time})")
 //	int insertMessage(InterviewMessage interviewMessage);
